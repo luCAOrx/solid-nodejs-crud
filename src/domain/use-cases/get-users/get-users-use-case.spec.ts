00a805ebd7e7a0278e7b5607966d9278a8a71f48
luCAOrx <lucaorx@protonmail.com>
2023-06-17 01:01:40 -0300

    test: Add use case test to get users

---
+++ src/domain/use-cases/get-users/get-users-use-case.spec.ts
@@ -0,0 +1,51 @@
+import { type Email } from "@domain/entities/email";
+import { type Job } from "@domain/entities/job";
+import { type Name } from "@domain/entities/name";
+import { type Password } from "@domain/entities/password";
+import { User } from "@domain/entities/user";
+import { InMemoryUserDatabase } from "@test/in-memory-database/in-memory-user-database";
+
+import { GetUsersUseCase } from "./get-users-use-case";
+
+describe("Get users use case", () => {
+  const inMemoryUserDatabase = new InMemoryUserDatabase();
+  const getUsersUseCase = new GetUsersUseCase(inMemoryUserDatabase);
+
+  beforeAll(async () => {
+    for (let i = 0; i < 20; i++) {
+      const user = User.create({
+        name: "Rafael" as unknown as Name,
+        job: "Actor" as unknown as Job,
+        email: `fernando${i}@example.com` as unknown as Email,
+        password: "1234567890" as unknown as Password,
+      });
+
+      await inMemoryUserDatabase.create(user);
+    }
+  });
+
+  it("should be able list users", async () => {
+    const usersOrUser = await getUsersUseCase.execute({});
+
+    expect(usersOrUser.length).toEqual(5);
+    expect(usersOrUser[0].props.email).toStrictEqual("fernando0@example.com");
+    expect(usersOrUser[1].props.email).toStrictEqual("fernando1@example.com");
+    expect(usersOrUser).toBeInstanceOf(Array<User>);
+  });
+
+  it("should be able paginate", async () => {
+    let usersOrUser = await getUsersUseCase.execute({ takePage: 5 });
+
+    expect(usersOrUser.length).toEqual(5);
+    expect(usersOrUser[0].props.email).toStrictEqual("fernando0@example.com");
+    expect(usersOrUser[1].props.email).toStrictEqual("fernando1@example.com");
+    expect(usersOrUser).toBeInstanceOf(Array<User>);
+
+    usersOrUser = await getUsersUseCase.execute({ page: 2, takePage: 5 });
+
+    expect(usersOrUser.length).toEqual(5);
+    expect(usersOrUser[0].props.email).toStrictEqual("fernando5@example.com");
+    expect(usersOrUser[1].props.email).toStrictEqual("fernando6@example.com");
+    expect(usersOrUser).toBeInstanceOf(Array<User>);
+  });
+});
