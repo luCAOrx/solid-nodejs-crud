5809add293b6214730f54e5a7e368fd8c73af6e4
luCAOrx <lucaorx@protonmail.com>
2023-06-17 01:01:53 -0300

    feat: Add use case to get users

---
+++ src/domain/use-cases/get-users/get-users-use-case.ts
@@ -0,0 +1,17 @@
+import { type User } from "@domain/entities/user";
+import { type UserRepository } from "@domain/repositories/user-repository";
+
+interface GetUsersRequest {
+  page?: number;
+  takePage?: number;
+}
+
+export class GetUsersUseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute({ page = 1, takePage = 5 }: GetUsersRequest): Promise<User[]> {
+    const usersOrUser = await this.userRepository.findMany(page, takePage);
+
+    return usersOrUser;
+  }
+}
