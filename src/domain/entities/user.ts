6c7623a26d7254d3071f1b13cbccfba55cacfa32
luCAOrx <lucaorx@protonmail.com>
2023-06-19 04:41:41 -0300

    chore: Added read_time, updated_at, created_at properties and their getters and setters methods

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -10,6 +10,9 @@ interface UserProps {
 export class User {
   private readonly _id: string;
   private _props: UserProps;
+  private _read_time: number;
+  private _updated_at: Date;
+  private readonly _created_at: Date;
 
   public get id(): string {
     return this._id;
@@ -23,12 +26,45 @@ export class User {
     this._props = value;
   }
 
-  private constructor(props: UserProps, id?: string) {
-    this._id = id ?? randomUUID();
-    this._props = props;
+  public get created_at(): Date {
+    return this._created_at;
   }
 
-  static create(props: UserProps, id?: string): User {
-    return new User(props, id);
+  public get read_time(): number {
+    return this._read_time;
+  }
+
+  public set read_time(value: number) {
+    this._read_time = value;
+  }
+
+  public get updated_at(): Date {
+    return this._updated_at;
+  }
+
+  public set updated_at(value: Date) {
+    this._updated_at = value;
+  }
+
+  private constructor(
+    props: UserProps,
+    id?: string,
+    read_time?: number,
+    updated_at?: Date
+  ) {
+    this._id = id ?? randomUUID();
+    this._props = props;
+    this._read_time = read_time ?? 0;
+    this._updated_at = updated_at ?? new Date();
+    this._created_at = new Date();
+  }
+
+  static create(
+    props: UserProps,
+    id?: string,
+    read_time?: number,
+    updated_at?: Date
+  ): User {
+    return new User(props, id, read_time, updated_at);
   }
 }
