0c4d1bc7abec0328b8f906e2f8046cf7b461e2b5
luCAOrx <lucaorx@protonmail.com>
2023-06-19 23:22:22 -0300

    chore: Add user repository

---
+++ src/domain/repositories/user-repository.ts
@@ -0,0 +1,11 @@
+import { type User } from "@domain/entities/user";
+
+export abstract class UserRepository {
+  abstract exists(email: string): Promise<boolean>;
+  abstract create(user: User): Promise<User>;
+  abstract findMany(page: number, takePage: number): Promise<User[]>;
+  abstract findById(id: string): Promise<User | null>;
+  abstract findByEmail(email: string): Promise<User | null>;
+  abstract update(user: User): Promise<User>;
+  abstract delete(id: string): Promise<void>;
+}
