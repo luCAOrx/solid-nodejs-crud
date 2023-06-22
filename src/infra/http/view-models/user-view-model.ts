bd83b984bf71814d74714fe9d803afcd3422ddeb
luCAOrx <lucaorx@protonmail.com>
2023-06-21 23:36:00 -0300

    chore: Add method to handle viewing a user return to htpp

---
+++ src/infra/http/view-models/user-view-model.ts
@@ -0,0 +1,31 @@
+import { type User } from "@domain/entities/user";
+
+export interface ToHttpResponse {
+  id: string;
+  name: string;
+  job: string;
+  email: string;
+  read_time?: number;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export class UserViewModel {
+  static toHttp({
+    id,
+    props: { name, job, email, password },
+    read_time,
+    created_at,
+    updated_at,
+  }: User): ToHttpResponse {
+    return {
+      id,
+      name,
+      job,
+      email,
+      read_time,
+      created_at,
+      updated_at,
+    };
+  }
+}
