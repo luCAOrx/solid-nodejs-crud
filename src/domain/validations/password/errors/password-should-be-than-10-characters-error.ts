794c40d4e42fdf1d3a983f242dd4eda616c77d4c
luCAOrx <lucaorx@protonmail.com>
2023-06-21 22:25:43 -0300

    chore: Changed error message

--- src/domain/validations/password/errors/password-should-be-than-10-characters-error.ts
+++ src/domain/validations/password/errors/password-should-be-than-10-characters-error.ts
@@ -1,6 +1,6 @@
 export class PasswordShouldBeThan10CharactersError extends Error {
   constructor() {
-    super("The field password should be than 5 characters");
+    super("The field password should be than 10 characters");
     this.name = "PasswordShouldBeThan10CharactersError";
   }
 }
