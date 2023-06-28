17c32203130cac378637e2706baf54cbbaecabd4
luCAOrx <lucaorx@protonmail.com>
2023-06-28 00:58:13 -0300

    chore: Add global middleware for fetch a user, fetch users, update user and delete user routes

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -5,10 +5,18 @@ import "express-async-errors";
 import cors from "cors";
 import express from "express";
 
+import { deleteUserRoute } from "./routes/delete-user.routes";
+import { getUserRoute } from "./routes/get-user.routes";
+import { getUsersRoute } from "./routes/get-users.routes";
 import { registerUserRoute } from "./routes/register-user.routes";
+import { updateUserRoute } from "./routes/update-user.routes";
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(registerUserRoute);
+app.use(getUserRoute);
+app.use(getUsersRoute);
+app.use(updateUserRoute);
+app.use(deleteUserRoute);
