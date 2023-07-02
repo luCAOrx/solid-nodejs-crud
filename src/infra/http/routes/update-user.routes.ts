8198744c1aec565bb5856a76096d3b4fd1db5795
luCAOrx <lucaorx@protonmail.com>
2023-07-02 00:27:39 -0300

    feat: Add middleware to ensure authentication

--- src/infra/http/routes/update-user.routes.ts
+++ src/infra/http/routes/update-user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import { UpdateUserController } from "../controllers/update-user/update-user-controller";
+import { EnsureAuthenticated } from "../middlewares/ensure-authenticated-middleware";
 
 export const updateUserRoute = Router();
 
 updateUserRoute.put(
   "/users/update-user/:id",
+  new EnsureAuthenticated().handle,
   new UpdateUserController().handle
 );
