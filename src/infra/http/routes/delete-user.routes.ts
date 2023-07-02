8e09c624d90a6f470f0d3b3c9978a4e60ff304a4
luCAOrx <lucaorx@protonmail.com>
2023-07-02 00:27:24 -0300

    feat: Add middleware to ensure authentication

--- src/infra/http/routes/delete-user.routes.ts
+++ src/infra/http/routes/delete-user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import { DeleteUserController } from "../controllers/delete-user/delete-user-controller";
+import { EnsureAuthenticated } from "../middlewares/ensure-authenticated-middleware";
 
 export const deleteUserRoute = Router();
 
 deleteUserRoute.delete(
   "/users/delete-user/:id",
+  new EnsureAuthenticated().handle,
   new DeleteUserController().handle
 );
