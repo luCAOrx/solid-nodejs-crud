fadff4a872929aa44ba820a4456f87cd166e431f
luCAOrx <lucaorx@protonmail.com>
2023-07-02 00:24:24 -0300

    chore: Encapsulated code around try catch, add password validation, and add password hashing

--- src/infra/http/controllers/register-user/register-user-controller.ts
+++ src/infra/http/controllers/register-user/register-user-controller.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { type Request, type Response } from "express";
 
 import { UserAlreadyExistsError } from "@domain/use-cases/register-user/errors/user-already-exists-error";
@@ -13,50 +14,57 @@ import { NameShouldNotBeEmptyError } from "@domain/validations/name/errors/name-
 import { PasswordShouldBeLessThan255CharactersError } from "@domain/validations/password/errors/password-should-be-less-than-255-characters-error";
 import { PasswordShouldBeThan10CharactersError } from "@domain/validations/password/errors/password-should-be-than-10-characters-error";
 import { PasswordShouldNotBeEmptyError } from "@domain/validations/password/errors/password-should-not-be-empty-error";
+import { PasswordValidation } from "@domain/validations/password/password-validation";
 import { PrismaUserRepository } from "@infra/http/repositories/prisma-user-repository";
 import { UserViewModel } from "@infra/http/view-models/user-view-model";
 
 export class RegisterUserController {
-  async handle(request: Request, response: Response): Promise<void> {
+  async handle(
+    request: Request,
+    response: Response
+  ): Promise<Response<any, Record<string, any>> | undefined> {
     const { name, job, email, password } = request.body;
 
     const prismaUserRepository = new PrismaUserRepository();
 
     const registerUserUseCase = new RegisterUserUseCase(prismaUserRepository);
 
-    await registerUserUseCase
-      .execute({
+    try {
+      PasswordValidation.valid(password);
+
+      const passwordHashed = await hash(password, 14);
+
+      const { user } = await registerUserUseCase.execute({
         name,
         job,
         email,
-        password,
-      })
-      .then(({ user }) => {
-        const userResponse = UserViewModel.toHttp(user);
-
-        return response.status(201).json({ user: userResponse });
-      })
-      .catch((error: Error) => {
-        if (
-          error instanceof UserAlreadyExistsError ||
-          error instanceof NameShouldNotBeEmptyError ||
-          error instanceof NameShouldBeLessThan255CharactersError ||
-          error instanceof NameShouldBeThan5CharactersError ||
-          error instanceof JobShouldNotBeEmptyError ||
-          error instanceof JobShouldBeLessThan255CharactersError ||
-          error instanceof JobShouldBeThan5CharactersError ||
-          error instanceof EmailShouldBeValidEmailError ||
-          error instanceof EmailShouldBeLessThan255CharactersError ||
-          error instanceof PasswordShouldNotBeEmptyError ||
-          error instanceof PasswordShouldBeLessThan255CharactersError ||
-          error instanceof PasswordShouldBeThan10CharactersError
-        ) {
-          return response.status(400).json({
-            statusCode: 400,
-            message: error.message,
-            error: "Bad request",
-          });
-        }
+        password: passwordHashed,
       });
+
+      const userResponse = UserViewModel.toHttp(user);
+
+      return response.status(201).json({ user: userResponse });
+    } catch (error) {
+      if (
+        error instanceof UserAlreadyExistsError ||
+        error instanceof NameShouldNotBeEmptyError ||
+        error instanceof NameShouldBeLessThan255CharactersError ||
+        error instanceof NameShouldBeThan5CharactersError ||
+        error instanceof JobShouldNotBeEmptyError ||
+        error instanceof JobShouldBeLessThan255CharactersError ||
+        error instanceof JobShouldBeThan5CharactersError ||
+        error instanceof EmailShouldBeValidEmailError ||
+        error instanceof EmailShouldBeLessThan255CharactersError ||
+        error instanceof PasswordShouldNotBeEmptyError ||
+        error instanceof PasswordShouldBeLessThan255CharactersError ||
+        error instanceof PasswordShouldBeThan10CharactersError
+      ) {
+        return response.status(400).json({
+          statusCode: 400,
+          message: error.message,
+          error: "Bad request",
+        });
+      }
+    }
   }
 }
