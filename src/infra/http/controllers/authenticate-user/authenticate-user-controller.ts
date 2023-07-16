8a36915ad37c215594ec12601f4153a59af5e569
luCAOrx <lucaorx@protonmail.com>
2023-07-16 17:57:18 -0300

    refactor: Added typing for the request body, wrapped the code around try catch, and added checking to see if the request body properties are being passed correctly

--- src/infra/http/controllers/authenticate-user/authenticate-user-controller.ts
+++ src/infra/http/controllers/authenticate-user/authenticate-user-controller.ts
@@ -7,43 +7,63 @@ import { GenerateTokenProvider } from "@infra/http/providers/generate-token-prov
 import { PrismaUserRepository } from "@infra/http/repositories/prisma-user-repository";
 import { UserViewModel } from "@infra/http/view-models/user-view-model";
 
+interface AuthenticateUserRequestBodyProps {
+  email: string;
+  password: string;
+}
+
 export class AuthenticateUserController {
-  async handle(request: Request, response: Response): Promise<void> {
-    const { email, password } = request.body;
+  async handle(
+    request: Request,
+    response: Response
+  ): Promise<Response<any, Record<string, any>> | undefined> {
+    const { email, password } =
+      request.body as AuthenticateUserRequestBodyProps;
 
     const prismaUserRepository = new PrismaUserRepository();
     const authenticateUserUseCase = new AuthenticateUserUseCase(
       prismaUserRepository
     );
 
-    const user = await prismaUserRepository.findByEmail(email);
+    try {
+      const userFounded = await prismaUserRepository.findByEmail(email);
 
-    const isPasswordSameSaveInDatabase = await compare(
-      password,
-      String(user?.props.password)
-    );
+      const isPasswordSameSaveInDatabase = await compare(
+        password,
+        String(userFounded?.props.password)
+      );
 
-    await authenticateUserUseCase
-      .execute({
+      const { user } = await authenticateUserUseCase.execute({
         email,
         password,
         isPasswordSameSaveInDatabase,
-      })
-      .then(({ user }) => {
-        const userResponse = UserViewModel.toHttp(user);
-
-        const token = GenerateTokenProvider.execute({ id: user.id });
-
-        return response.status(201).json({ user: userResponse, token });
-      })
-      .catch((error: Error) => {
-        if (error instanceof InvalidEmailOrPasswordError) {
-          return response.status(400).json({
-            statusCode: 400,
-            message: error.message,
-            error: "Bad request",
-          });
-        }
       });
+      const userResponse = UserViewModel.toHttp(user);
+
+      const token = GenerateTokenProvider.execute({ id: user.id });
+
+      return response.status(201).json({ user: userResponse, token });
+    } catch (error) {
+      if (error instanceof InvalidEmailOrPasswordError) {
+        return response.status(400).json({
+          statusCode: 400,
+          message: error.message,
+          error: "Bad request",
+        });
+      }
+
+      if (
+        Object.keys(request.body).length === 0 ||
+        Object.hasOwn(request.body, "email") ||
+        Object.hasOwn(request.body, "password")
+      ) {
+        return response.status(500).json({
+          statusCode: 500,
+          message:
+            "The properties: email and password, should be provided in the request body",
+          error: "Internal Server Error",
+        });
+      }
+    }
   }
 }
