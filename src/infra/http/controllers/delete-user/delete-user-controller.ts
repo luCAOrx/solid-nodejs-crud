cf994cbcceafaa74f545d12cdd611fc35367071e
luCAOrx <lucaorx@protonmail.com>
2023-07-16 18:00:03 -0300

    chore: Add typing for route params

--- src/infra/http/controllers/delete-user/delete-user-controller.ts
+++ src/infra/http/controllers/delete-user/delete-user-controller.ts
@@ -4,9 +4,13 @@ import { DeleteUserUseCase } from "@domain/use-cases/delete-user/delete-user-use
 import { UserNotFoundError } from "@domain/use-cases/errors/user-not-found-error";
 import { PrismaUserRepository } from "@infra/http/repositories/prisma-user-repository";
 
+interface DeleteUserRouteParamsProps {
+  id: string;
+}
+
 export class DeleteUserController {
   async handle(request: Request, response: Response): Promise<void> {
-    const { id } = request.params;
+    const { id } = request.params as unknown as DeleteUserRouteParamsProps;
 
     const prismaUserRepository = new PrismaUserRepository();
     const deleteUserUseCase = new DeleteUserUseCase(prismaUserRepository);
