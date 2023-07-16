80054062e69029fa4f5afa58717aa0bfb8548883
luCAOrx <lucaorx@protonmail.com>
2023-07-16 18:09:42 -0300

    refactor: Add typing for the query parameter, and added checking to see if the properties provided in request.query are being passed

--- src/infra/http/controllers/get-users/get-users-controller.ts
+++ src/infra/http/controllers/get-users/get-users-controller.ts
@@ -4,9 +4,15 @@ import { GetUsersUseCase } from "@domain/use-cases/get-users/get-users-use-case"
 import { PrismaUserRepository } from "@infra/http/repositories/prisma-user-repository";
 import { UserViewModel } from "@infra/http/view-models/user-view-model";
 
+interface GetUserQueryParamsProps {
+  page: string;
+  takePage: string;
+}
+
 export class GetUsersController {
   async handle(request: Request, response: Response): Promise<void> {
-    const { page, takePage } = request.query;
+    const { page, takePage } =
+      request.query as unknown as GetUserQueryParamsProps;
 
     const prismaUserRepository = new PrismaUserRepository();
     const getUsersUseCase = new GetUsersUseCase(prismaUserRepository);
@@ -20,13 +26,25 @@ export class GetUsersController {
 
         return response.status(200).json({ userOrUsers });
       })
-      .catch((error: Error) => {
-        error ??
-          response.status(400).json({
-            statusCode: 400,
-            message: "Error listing users",
-            error: "Bad request",
+      .catch(() => {
+        if (
+          Object.keys(request.query).length === 0 ||
+          Object.hasOwn(request.query, "page") ||
+          Object.hasOwn(request.query, "takePage")
+        ) {
+          return response.status(500).json({
+            statusCode: 500,
+            message:
+              "The query parameters: page and takePage, must be provided in the query parameters of the request",
+            error: "Internal Server Error",
           });
+        }
+
+        return response.status(400).json({
+          statusCode: 400,
+          message: "Error listing users",
+          error: "Bad request",
+        });
       });
   }
 }
