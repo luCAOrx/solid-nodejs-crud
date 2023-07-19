898e336448ac93e222da0115ad1ee98138f85c8c
luCAOrx <lucaorx@protonmail.com>
2023-07-19 17:16:33 -0300

    chore: Add user directory with your unit test and entity

---
+++ src/domain/entities/user/user.spec.ts
@@ -0,0 +1,27 @@
+import { ok, strictEqual } from "node:assert";
+import { describe, it } from "node:test";
+
+import { Email } from "../email/email";
+import { Job } from "../job/job";
+import { Name } from "../name/name";
+import { Password } from "../password/password";
+import { User } from "./user";
+
+const name = Name.create("Lucas").value;
+const job = Job.create("Development").value;
+const email = Email.create("lucas@example.com").value;
+const password = Password.create("1234567890").value;
+
+describe("User model", () => {
+  it("should be able to create a new user", () => {
+    const userOrError = User.create({
+      name,
+      job,
+      email,
+      password,
+    });
+
+    ok(userOrError);
+    strictEqual(userOrError, userOrError);
+  });
+});
---
+++ src/domain/entities/user/user.ts
@@ -0,0 +1,70 @@
+import { randomUUID } from "node:crypto";
+
+interface UserProps {
+  name: string;
+  job: string;
+  email: string;
+  password: string;
+}
+
+export class User {
+  private readonly _id: string;
+  private _props: UserProps;
+  private _read_time: number;
+  private _updated_at: Date;
+  private readonly _created_at: Date;
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get props(): UserProps {
+    return this._props;
+  }
+
+  public set props(value: UserProps) {
+    this._props = value;
+  }
+
+  public get created_at(): Date {
+    return this._created_at;
+  }
+
+  public get read_time(): number {
+    return this._read_time;
+  }
+
+  public set read_time(value: number) {
+    this._read_time = value;
+  }
+
+  public get updated_at(): Date {
+    return this._updated_at;
+  }
+
+  public set updated_at(value: Date) {
+    this._updated_at = value;
+  }
+
+  private constructor(
+    props: UserProps,
+    id?: string,
+    read_time?: number,
+    updated_at?: Date
+  ) {
+    this._id = id ?? randomUUID();
+    this._props = props;
+    this._read_time = read_time ?? 0;
+    this._updated_at = updated_at ?? new Date();
+    this._created_at = new Date();
+  }
+
+  static create(
+    props: UserProps,
+    id?: string,
+    read_time?: number,
+    updated_at?: Date
+  ): User {
+    return new User(props, id, read_time, updated_at);
+  }
+}
