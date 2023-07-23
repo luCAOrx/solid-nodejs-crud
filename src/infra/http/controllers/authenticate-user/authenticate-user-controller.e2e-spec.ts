11c90b86f68e6b9cf3c5d26c9b6a8b3207fa7335
luCAOrx <lucaorx@protonmail.com>
2023-07-23 16:25:08 -0300

    test: Changed jest test tool to node test runner

--- src/infra/http/controllers/authenticate-user/authenticate-user-controller.e2e-spec.ts
+++ src/infra/http/controllers/authenticate-user/authenticate-user-controller.e2e-spec.ts
@@ -1,143 +1,155 @@
-import request from "supertest";
+import { deepStrictEqual } from "node:assert";
+import { describe, it } from "node:test";
 
-import { app } from "@infra/http/app";
+import { MakeRequestLoginFactory } from "@test/factories/make-request-login-factory";
 import { MakeUserFactory } from "@test/factories/make-user-factory";
 
-describe("Authenticate user controller", () => {
-  it("should be able authenticate", async () => {
-    const { body } = await new MakeUserFactory().toHttp({
-      override: {
-        email: "test-auth@example.com",
-        password: "1234567890",
-      },
-    });
-
-    const {
-      body: authenticateUserBody,
-      statusCode: authenticateUserStatusCode,
-    } = await request(app).post("/users/authenticate").send({
-      email: "test-auth@example.com",
-      password: "1234567890",
-    });
-
-    expect(authenticateUserStatusCode).toStrictEqual(201);
-    expect(authenticateUserBody).toStrictEqual({
-      user: {
-        id: body.user.id,
-        name: body.user.name,
-        job: body.user.job,
-        email: body.user.email,
-        read_time: body.user.read_time,
-        created_at: authenticateUserBody.user.created_at,
-        updated_at: authenticateUserBody.user.updated_at,
-      },
-      token: authenticateUserBody.token,
-    });
-  });
-
-  it("should not be able to authenticate user without properties of request body", async () => {
-    await new MakeUserFactory().toHttp({
-      override: {
-        email: "test@example.com",
-        password: "1234567890",
-      },
-    });
-
-    await request(app).post("/users/authenticate").send({}).expect(500, {
-      statusCode: 500,
-      message:
-        "The properties: email and password, should be provided in the request body",
-      error: "Internal Server Error",
-    });
-  });
-
-  it("should not be able to authenticate user just with email property of request body", async () => {
-    await new MakeUserFactory().toHttp({
-      override: {
-        email: "test@example.com",
-        password: "1234567890",
-      },
-    });
-
-    await request(app)
-      .post("/users/authenticate")
-      .send({
-        email: "test@example.com",
-      })
-      .expect(500, {
-        statusCode: 500,
-        message:
-          "The properties: email and password, should be provided in the request body",
-        error: "Internal Server Error",
+export function authenticateUserControllerEndToEndTests(): void {
+  describe("Authenticate user controller", () => {
+    it("should be able authenticate", async () => {
+      await new MakeUserFactory().toHttp({
+        override: {
+          email: "test-auth@example.com",
+          password: "1234567890",
+        },
       });
-  });
 
-  it("should not be able to authenticate user just with password property of request body", async () => {
-    await new MakeUserFactory().toHttp({
-      override: {
-        email: "test@example.com",
-        password: "1234567890",
-      },
-    });
+      await MakeRequestLoginFactory.execute({
+        data: {
+          email: "test-auth@example.com",
+          password: "1234567890",
+        },
+      }).then(async (response) => {
+        const responseBody = await response.json();
 
-    await request(app)
-      .post("/users/authenticate")
-      .send({
-        password: "1234567890",
-      })
-      .expect(500, {
-        statusCode: 500,
-        message:
-          "The properties: email and password, should be provided in the request body",
-        error: "Internal Server Error",
+        deepStrictEqual(response.status, 201);
+        deepStrictEqual(responseBody, {
+          user: {
+            id: responseBody.user.id,
+            name: "John Doe",
+            job: "doctor",
+            email: "test-auth@example.com",
+            read_time: 0,
+            created_at: responseBody.user.created_at,
+            updated_at: responseBody.user.updated_at,
+          },
+          token: responseBody.token,
+        });
       });
+    });
+
+    it("should not be able to authenticate user without properties of request body", async () => {
+      await MakeRequestLoginFactory.execute({
+        data: {},
+      }).then(async (response) => {
+        const responseBody = await response.json();
+
+        deepStrictEqual(response.status, 500);
+        deepStrictEqual(responseBody, {
+          statusCode: 500,
+          message:
+            "The properties: email and password, should be provided in the request body",
+          error: "Internal Server Error",
+        });
+      });
+    });
+
+    it("should not be able to authenticate user just with email property of request body", async () => {
+      await new MakeUserFactory().toHttp({
+        override: {
+          email: "test@example.com",
+          password: "1234567890",
+        },
+      });
+
+      await MakeRequestLoginFactory.execute({
+        data: { email: "test@example.com" },
+      }).then(async (response) => {
+        const responseBody = await response.json();
+
+        deepStrictEqual(response.status, 500);
+        deepStrictEqual(responseBody, {
+          statusCode: 500,
+          message:
+            "The properties: email and password, should be provided in the request body",
+          error: "Internal Server Error",
+        });
+      });
+    });
+
+    it("should not be able to authenticate user just with password property of request body", async () => {
+      await new MakeUserFactory().toHttp({
+        override: {
+          email: "test-auth1@example.com",
+          password: "1234567890",
+        },
+      });
+
+      await MakeRequestLoginFactory.execute({
+        data: {
+          password: "1234567890",
+        },
+      }).then(async (response) => {
+        const responseBody = await response.json();
+
+        deepStrictEqual(response.status, 500);
+        deepStrictEqual(responseBody, {
+          statusCode: 500,
+          message:
+            "The properties: email and password, should be provided in the request body",
+          error: "Internal Server Error",
+        });
+      });
+    });
+
+    it("should not be able authenticate if provided email not equal that user email", async () => {
+      await new MakeUserFactory().toHttp({
+        override: {
+          email: "test-auth2@example.com",
+          password: "1234567890",
+        },
+      });
+
+      await MakeRequestLoginFactory.execute({
+        data: {
+          email: "fake-email@example.com",
+          password: "1234567890",
+        },
+      }).then(async (response) => {
+        const responseBody = await response.json();
+
+        deepStrictEqual(response.status, 400);
+        deepStrictEqual(responseBody, {
+          statusCode: 400,
+          message: "Invalid email or password",
+          error: "Bad request",
+        });
+      });
+    });
+
+    it("should not be able authenticate if provided password not equal that user password", async () => {
+      await new MakeUserFactory().toHttp({
+        override: {
+          email: "test-auth3@example.com",
+          password: "1234567890",
+        },
+      });
+
+      await MakeRequestLoginFactory.execute({
+        data: {
+          email: "test-auth3@example.com",
+          password: "fake-password",
+        },
+      }).then(async (response) => {
+        const responseBody = await response.json();
+
+        deepStrictEqual(response.status, 400);
+        deepStrictEqual(responseBody, {
+          statusCode: 400,
+          message: "Invalid email or password",
+          error: "Bad request",
+        });
+      });
+    });
   });
-
-  it("should not be able authenticate if provided email not equal that user email", async () => {
-    await new MakeUserFactory().toHttp({
-      override: {
-        email: "test-auth@example.com",
-        password: "1234567890",
-      },
-    });
-
-    const {
-      body: authenticateUserBody,
-      statusCode: authenticateUserStatusCode,
-    } = await request(app).post("/users/authenticate").send({
-      email: "fake-email@example.com",
-      password: "1234567890",
-    });
-
-    expect(authenticateUserStatusCode).toStrictEqual(400);
-    expect(authenticateUserBody).toStrictEqual({
-      statusCode: 400,
-      message: "Invalid email or password",
-      error: "Bad request",
-    });
-  });
-
-  it("should not be able authenticate if provided password not equal that user password", async () => {
-    await new MakeUserFactory().toHttp({
-      override: {
-        email: "test-auth@example.com",
-        password: "1234567890",
-      },
-    });
-
-    const {
-      body: authenticateUserBody,
-      statusCode: authenticateUserStatusCode,
-    } = await request(app).post("/users/authenticate").send({
-      email: "test-auth@example.com",
-      password: "fake-password",
-    });
-
-    expect(authenticateUserStatusCode).toStrictEqual(400);
-    expect(authenticateUserBody).toStrictEqual({
-      statusCode: 400,
-      message: "Invalid email or password",
-      error: "Bad request",
-    });
-  });
-});
+}
