cfd0b515b84953bb4725f8f16ceb3d40dcd48334
luCAOrx <lucaorx@protonmail.com>
2023-07-23 16:31:19 -0300

    refactor: Add server variable export and removed console.log

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,5 +1,3 @@
 import { app } from "./app";
 
-app.listen(process.env.PORT ?? process.env.SERVER_PORT, () => {
-  console.log("Express server listening on port 3333");
-});
+export const server = app.listen(process.env.PORT ?? process.env.SERVER_PORT);
