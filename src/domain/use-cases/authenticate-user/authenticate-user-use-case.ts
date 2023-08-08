3f2717983831251b2a2ff7b47bd0b46c0463fcec
luCAOrx <lucaorx@protonmail.com>
2023-08-08 19:40:34 -0300

    feat: Add dependency inversion principle for security provider

--- src/domain/use-cases/authenticate-user/authenticate-user-use-case.ts
+++ src/domain/use-cases/authenticate-user/authenticate-user-use-case.ts
@@ -1,4 +1,5 @@
 import { type User } from "@domain/entities/user/user";
+import { type SecurityProvider } from "@domain/providers/security-provider";
 import { type UserRepository } from "@domain/repositories/user-repository";
 
 import { InvalidEmailOrPasswordError } from "./errors/invalid-email-or-password-error";
@@ -6,27 +7,39 @@ import { InvalidEmailOrPasswordError } from "./errors/invalid-email-or-password-
 interface AuthenticateUserRequest {
   email: string;
   password: string;
-  isPasswordSameSaveInDatabase: boolean;
 }
 
 interface AuthenticateUserResponse {
   user: User;
+  token: string;
 }
 
 export class AuthenticateUserUseCase {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly securityProvider: SecurityProvider
+  ) {}
 
   async execute({
     email,
     password,
-    isPasswordSameSaveInDatabase = false,
   }: AuthenticateUserRequest): Promise<AuthenticateUserResponse> {
     const userOrNull = await this.userRepository.findByEmail(email);
 
-    if (userOrNull === null) throw new InvalidEmailOrPasswordError();
+    const isPasswordSameSaveInDatabase = await this.securityProvider.compare({
+      password,
+      hash: String(userOrNull?.props.password),
+    });
 
-    if (!isPasswordSameSaveInDatabase) throw new InvalidEmailOrPasswordError();
+    if (userOrNull === null || !isPasswordSameSaveInDatabase)
+      throw new InvalidEmailOrPasswordError();
 
-    return { user: userOrNull };
+    const token = this.securityProvider.sign({
+      payload: { id: userOrNull.id },
+      secretOrPrivateKey: String(process.env.JWT_SECRET),
+      expiresIn: "20s",
+    });
+
+    return { user: userOrNull, token };
   }
 }
