9107ade4421d351246dc3b67744ec0cf9af492b6
luCAOrx <lucaorx@protonmail.com>
2023-08-08 19:55:09 -0300

    test: Add dependency to provide user security in use case constructor to register user

--- src/domain/use-cases/register-user/register-user-use-case.spec.ts
+++ src/domain/use-cases/register-user/register-user-use-case.spec.ts
@@ -4,12 +4,17 @@ import { describe, it } from "node:test";
 import { User } from "@domain/entities/user/user";
 import { NameShouldNotBeEmptyError } from "@domain/validations/name/errors/name-should-not-be-empty-error";
 import { InMemoryUserDatabase } from "@test/in-memory-database/in-memory-user-database";
+import { UserSecurityProvider } from "@test/utils/user-security-provider";
 
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 import { RegisterUserUseCase } from "./register-user-use-case";
 
 const inMemoryUserDatabase = new InMemoryUserDatabase();
-const registerUserUseCase = new RegisterUserUseCase(inMemoryUserDatabase);
+const userSecurityProvider = new UserSecurityProvider();
+const registerUserUseCase = new RegisterUserUseCase(
+  inMemoryUserDatabase,
+  userSecurityProvider
+);
 
 describe("Register user use case", () => {
   it("should be able to register new user", async () => {
