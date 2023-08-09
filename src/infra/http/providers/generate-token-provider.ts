a068331aa7abcc040d4761238c75985020d2b5ab
luCAOrx <lucaorx@protonmail.com>
2023-08-09 02:08:46 -0300

    chore: Changed file name from generate-token-provider to user-security-provider

--- src/infra/http/providers/generate-token-provider.ts
+++
@@ -1,9 +0,0 @@
-import { sign } from "jsonwebtoken";
-
-export class GenerateTokenProvider {
-  static execute(params: object): string {
-    return sign(params, String(process.env.JWT_SECRET), {
-      expiresIn: "20s",
-    });
-  }
-}
