f5e007233044abd564da82989f1a1b8511894cb9
luCAOrx <lucaorx@protonmail.com>
2023-09-03 21:34:40 -0300

    test: Changed the url of the execute method of the MakeRequestFactory class

--- src/infra/http/errors/page-not-found/page-not-found-error.e2e-spec.ts
+++ src/infra/http/errors/page-not-found/page-not-found-error.e2e-spec.ts
@@ -2,13 +2,12 @@ import { deepStrictEqual } from "node:assert";
 import { describe, it } from "node:test";
 
 import { MakeRequestFactory } from "@test/factories/make-request-factory";
-import { BASE_URL } from "@test/utils/base-url";
 
 export function pageNotFoundErrorEndToEndTests(): void {
   describe("Page not found error", () => {
     it("should be able return 404 not found error", async () => {
       await MakeRequestFactory.execute({
-        url: `${BASE_URL}/fake-route`,
+        url: `${String(process.env.TEST_SERVER_URL)}/fake-route`,
         method: "GET",
         headers: {
           "Content-Type": "application/json",
