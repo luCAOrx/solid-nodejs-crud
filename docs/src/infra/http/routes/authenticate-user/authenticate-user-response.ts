bf467c1bce48cf5223e1bf61c2c7bb4c853f2b1c
luCAOrx <lucaorx@protonmail.com>
2023-09-07 17:02:00 -0300

    docs: added example for response when user is authenticated, added example for invalid email or password error

--- docs/src/infra/http/routes/authenticate-user/authenticate-user-response.ts
+++ docs/src/infra/http/routes/authenticate-user/authenticate-user-response.ts
@@ -1,6 +1,27 @@
 import { type Responses, type Response } from "swagger-jsdoc";
 
 const httpResponseToAuthenticateUser: Response = {
+  description: "HTTP response to authenticate a user",
+  content: {
+    "application/json": {
+      example: {
+        user: {
+          id: "e6983148-565c-45f2-bcb4-8862cb64269a",
+          name: "John Doe",
+          job: "development",
+          email: "johndoe@example.com",
+          read_time: 0,
+          created_at: "2023-09-07T14:36:45.045Z",
+          updated_at: "2023-09-07T14:36:45.045Z",
+        },
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU2Njk2NGUyLTEyMGUtNDE4Zi1iYjgxLWVkYzEyZDYwMjFhNiIsImlhdCI6MTY5NDA5NzY4MCwiZXhwIjoxNjk0MDk3NzAwfQ.jZaJ32Ws_AOcOK4XIB_b0R15b8om0hXtJA2MpqqgXnc",
+      },
+    },
+  },
+};
+
+const httpResponseToClientError: Response = {
   description: "HTTP response to error in authenticate a user",
   content: {
     "application/json": {
@@ -30,6 +51,8 @@ const httpResponseToInternalServerError: Response = {
 
 export const authenticateUserResponse: Responses = {
   200: httpResponseToAuthenticateUser,
+
+  400: httpResponseToClientError,
 
   500: httpResponseToInternalServerError,
 };
