5d64cec1208081bc39f02dfd9250e6d0a7493963
luCAOrx <lucaorx@protonmail.com>
2023-09-07 17:11:49 -0300

    docs: add authentication and parameter in route

--- docs/src/infra/http/routes/get-users/get-users-request.ts
+++ docs/src/infra/http/routes/get-users/get-users-request.ts
@@ -6,7 +6,17 @@ export const getUsersRequest: Operation = {
   tags: ["User"],
   summary: "Get users",
   description: "This route get 5 users per page",
+  security: [{ bearerAuth: [] }],
   parameters: [
+    {
+      in: "path",
+      name: "id",
+      required: true,
+      schema: {
+        $ref: "#/components/schemas/UserRouteParams",
+      },
+      example: "04deb5b7-8fe0-4885-9dac-ac2683d89e75",
+    },
     {
       in: "query",
       name: "page",
