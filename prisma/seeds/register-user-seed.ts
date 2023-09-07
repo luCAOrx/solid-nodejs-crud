6b5623db4d9ccfe7b391d4a93c3af451f54020cf
luCAOrx <lucaorx@protonmail.com>
2023-09-07 17:51:22 -0300

    chore: add seed for user creation in database

---
+++ prisma/seeds/register-user-seed.ts
@@ -0,0 +1,44 @@
+import { hash } from "bcryptjs";
+
+import { User } from "@domain/entities/user/user";
+import { prisma } from "@infra/http/libs/prisma-client";
+async function main(): Promise<void> {
+  const {
+    id,
+    props: { name, job, email, password, role },
+    read_time,
+    created_at,
+  } = User.create({
+    name: "Admin Name",
+    job: "admin",
+    email: String(process.env.ADMIN_EMAIL),
+    password: String(process.env.ADMIN_PASSWORD),
+    role: "ADMIN",
+  });
+
+  const hashedPassword = await hash(password, 14);
+
+  await prisma.user.create({
+    data: {
+      id,
+      name,
+      job,
+      email,
+      password: hashedPassword,
+      role,
+      read_time,
+      created_at,
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (error) => {
+    console.log(error);
+
+    await prisma.$disconnect();
+    process.exit(1);
+  });
