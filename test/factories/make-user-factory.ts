14a5184ad5a845e4d384860d6748487562aa59ae
luCAOrx <lucaorx@protonmail.com>
2023-09-07 18:32:09 -0300

    test: add class to create admin user for domain and for HTTP

--- test/factories/make-user-factory.ts
+++ test/factories/make-user-factory.ts
@@ -1,5 +1,9 @@
-import { type Role, type User } from "@domain/entities/user/user";
+import { hash } from "bcryptjs";
+
+import { User, type Role } from "@domain/entities/user/user";
 import { RegisterUserUseCase } from "@domain/use-cases/register-user/register-user-use-case";
+import { prisma } from "@infra/http/libs/prisma-client";
+import { type User as UserPrismaClient } from "@prisma/client";
 import { InMemoryUserDatabase } from "@test/in-memory-database/in-memory-user-database";
 import { UserSecurityProvider } from "@test/utils/user-security-provider";
 
@@ -62,5 +66,65 @@ export class MakeUserFactory {
         ...override,
       },
     });
+  }
+}
+
+export class MakeAdminUserFactory {
+  public toDomain({
+    inMemoryDatabase,
+    override,
+  }: {
+    inMemoryDatabase: InMemoryUserDatabase;
+    override?: Override;
+  }): User {
+    const user = User.create({
+      name: "David Wilson",
+      job: "Manager",
+      email: "david@example.com",
+      password: "1234567890",
+      role: "ADMIN",
+      ...override,
+    });
+
+    inMemoryDatabase.create(user);
+
+    return user;
+  }
+
+  public async toHttp({
+    override,
+  }: {
+    override?: Override;
+  }): Promise<UserPrismaClient> {
+    const {
+      id,
+      props: { name, job, email, password, role },
+      read_time,
+      created_at,
+    } = User.create({
+      name: "Test Admin Name",
+      job: "test admin",
+      email: "test-admin@example.com",
+      password: "1234567890",
+      role: "ADMIN",
+      ...override,
+    });
+
+    const hashedPassword = await hash(password, 14);
+
+    const createdUser = await prisma.user.create({
+      data: {
+        id,
+        name,
+        job,
+        email,
+        password: hashedPassword,
+        role,
+        read_time,
+        created_at,
+      },
+    });
+
+    return createdUser;
   }
 }
