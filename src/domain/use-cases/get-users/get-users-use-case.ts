98e5ab11cc466a36e084ff099e243682d98cdd16
luCAOrx <lucaorx@protonmail.com>
2023-09-07 18:03:46 -0300

    refactor: added id to the execute method parameter, added business rule to not allow listing users if the user you want to list does not exist and added business rule to not allow listing users if the user you want to list is not an administrator

--- src/domain/use-cases/get-users/get-users-use-case.ts
+++ src/domain/use-cases/get-users/get-users-use-case.ts
@@ -1,7 +1,11 @@
 import { type User } from "@domain/entities/user/user";
 import { type UserRepository } from "@domain/repositories/user-repository";
 
+import { UserNotFoundError } from "../errors/user-not-found-error";
+import { AccessDeniedError } from "./errors/access-denied-error";
+
 interface GetUsersRequest {
+  id: string;
   page?: number;
   takePage?: number;
 }
@@ -9,7 +13,17 @@ interface GetUsersRequest {
 export class GetUsersUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute({ page = 1, takePage = 5 }: GetUsersRequest): Promise<User[]> {
+  async execute({
+    id,
+    page = 1,
+    takePage = 5,
+  }: GetUsersRequest): Promise<User[]> {
+    const user = await this.userRepository.findById(id);
+
+    if (user === null) throw new UserNotFoundError();
+
+    if (user.props.role !== "ADMIN") throw new AccessDeniedError();
+
     const usersOrUser = await this.userRepository.findMany(page, takePage);
 
     return usersOrUser;
