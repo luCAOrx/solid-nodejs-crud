a7725cecd69a929e8da3a7d43befe9dc6d2e0fb8
luCAOrx <lucaorx@protonmail.com>
2023-09-07 17:59:36 -0300

    test: changed the title of the test to list users, added tests to not allow listing users if the user you want to list does not exist and added tests to not allow listing users if the user you want to list is not admin

--- src/domain/use-cases/get-users/get-users-use-case.spec.ts
+++ src/domain/use-cases/get-users/get-users-use-case.spec.ts
@@ -1,11 +1,14 @@
-import { strictEqual, ok } from "node:assert";
+import { strictEqual, ok, rejects } from "node:assert";
 import { describe, it, before } from "node:test";
 
 import { type User } from "@domain/entities/user/user";
+import { MakeAdminUserFactory } from "@test/factories/make-user-factory";
 import { InMemoryUserDatabase } from "@test/in-memory-database/in-memory-user-database";
 import { UserSecurityProvider } from "@test/utils/user-security-provider";
 
+import { UserNotFoundError } from "../errors/user-not-found-error";
 import { RegisterUserUseCase } from "../register-user/register-user-use-case";
+import { AccessDeniedError } from "./errors/access-denied-error";
 import { GetUsersUseCase } from "./get-users-use-case";
 
 describe("Get users use case", () => {
@@ -18,6 +21,10 @@ describe("Get users use case", () => {
 
   const getUsersUseCase = new GetUsersUseCase(inMemoryUserDatabase);
 
+  const userAdmin = new MakeAdminUserFactory().toDomain({
+    inMemoryDatabase: inMemoryUserDatabase,
+  });
+
   before(async () => {
     for (let i = 0; i < 20; i++) {
       await registerUserUseCase.execute({
@@ -29,28 +36,54 @@ describe("Get users use case", () => {
     }
   });
 
-  it("should be able list users", async () => {
-    const usersOrUser = await getUsersUseCase.execute({});
+  it("should be able to list users if role is admin", async () => {
+    const usersOrUser = await getUsersUseCase.execute({ id: userAdmin.id });
 
     strictEqual(usersOrUser.length, 5);
-    strictEqual(usersOrUser[0].props.email, "fernando0@example.com");
-    strictEqual(usersOrUser[1].props.email, "fernando1@example.com");
+    strictEqual(usersOrUser[0].props.email, "david@example.com");
+    strictEqual(usersOrUser[1].props.email, "fernando0@example.com");
     ok(usersOrUser instanceof Array<User>);
   });
 
+  it("should not be able to list users if user not found", async () => {
+    await rejects(async () => {
+      await getUsersUseCase.execute({ id: "1234567890" });
+    }, UserNotFoundError);
+  });
+
+  it("should not be able to list users if role is common", async () => {
+    const userCommon = await registerUserUseCase.execute({
+      name: "Gregory Schmidt",
+      job: "Soccer Player",
+      email: "jimble@example.com",
+      password: "1234567890",
+    });
+
+    await rejects(async () => {
+      await getUsersUseCase.execute({ id: userCommon.user.id });
+    }, AccessDeniedError);
+  });
+
   it("should be able paginate", async () => {
-    let usersOrUser = await getUsersUseCase.execute({ takePage: 5 });
+    let usersOrUser = await getUsersUseCase.execute({
+      id: userAdmin.id,
+      takePage: 5,
+    });
 
     strictEqual(usersOrUser.length, 5);
-    strictEqual(usersOrUser[0].props.email, "fernando0@example.com");
-    strictEqual(usersOrUser[1].props.email, "fernando1@example.com");
+    strictEqual(usersOrUser[0].props.email, "david@example.com");
+    strictEqual(usersOrUser[1].props.email, "fernando0@example.com");
     ok(usersOrUser instanceof Array<User>);
 
-    usersOrUser = await getUsersUseCase.execute({ page: 2, takePage: 5 });
+    usersOrUser = await getUsersUseCase.execute({
+      id: userAdmin.id,
+      page: 2,
+      takePage: 5,
+    });
 
     strictEqual(usersOrUser.length, 5);
-    strictEqual(usersOrUser[0].props.email, "fernando5@example.com");
-    strictEqual(usersOrUser[1].props.email, "fernando6@example.com");
+    strictEqual(usersOrUser[0].props.email, "fernando4@example.com");
+    strictEqual(usersOrUser[1].props.email, "fernando5@example.com");
     ok(usersOrUser instanceof Array<User>);
   });
 });
