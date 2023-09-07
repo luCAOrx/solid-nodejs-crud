ee710c2ecca4acddf634cb8e853c8b0e420b2c2e
luCAOrx <lucaorx@protonmail.com>
2023-09-07 18:26:00 -0300

    refactor: add route parameter and middleware to check if user is authenticated

--- src/infra/http/routes/get-users.routes.ts
+++ src/infra/http/routes/get-users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 
 import { GetUsersController } from "../controllers/get-users/get-users-controller";
+import { EnsureAuthenticated } from "../middlewares/ensure-authenticated-middleware";
 
 export const getUsersRoute = Router();
 
-getUsersRoute.get("/users/get-users", new GetUsersController().handle);
+getUsersRoute.get(
+  "/users/get-users/:id",
+  new EnsureAuthenticated().handle,
+  new GetUsersController().handle
+);
