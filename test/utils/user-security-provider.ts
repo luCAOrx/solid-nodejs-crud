fe1401d761dd4afd766b0c63e970f354f9d75ea1
luCAOrx <lucaorx@protonmail.com>
2023-09-21 22:55:52 -0300

    test: add refresh token method implementation

--- test/utils/user-security-provider.ts
+++ test/utils/user-security-provider.ts
@@ -6,6 +6,10 @@ import {
 } from "@domain/providers/security-provider";
 
 export class UserSecurityProvider implements SecurityProvider {
+  async refreshToken(userId: string): Promise<string> {
+    return "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+  }
+
   async compare({ password, hash }: CompareProps): Promise<boolean> {
     if (password === hash) return true;
 
