f3b4f62c1d14c4738eb346037ecb566c3123437e
luCAOrx <lucaorx@protonmail.com>
2023-09-21 22:42:24 -0300

    test: add comparison to check if the refresh token is following with the same authentication response data and in-memory database as the refresh token and add test to check if the existing refresh token is deleted if the user authenticates again

--- src/domain/use-cases/authenticate-user/authenticate-user-use-case.spec.ts
+++ src/domain/use-cases/authenticate-user/authenticate-user-use-case.spec.ts
@@ -1,7 +1,8 @@
-import { ok, rejects, strictEqual } from "node:assert";
+import { ok, rejects, strictEqual, notStrictEqual } from "node:assert";
 import { describe, it } from "node:test";
 
 import { MakeUserFactory } from "@test/factories/make-user-factory";
+import { InMemoryRefreshTokenDatabase } from "@test/in-memory-database/in-memory-refresh-token-database";
 import { InMemoryUserDatabase } from "@test/in-memory-database/in-memory-user-database";
 import { UserSecurityProvider } from "@test/utils/user-security-provider";
 
@@ -10,10 +11,12 @@ import { InvalidEmailOrPasswordError } from "./errors/invalid-email-or-password-
 
 describe("Authenticate user use case", () => {
   const inMemoryUserDatabase = new InMemoryUserDatabase();
+  const inMemoryRefreshTokenDatabase = new InMemoryRefreshTokenDatabase();
   const userSecurityProvider = new UserSecurityProvider();
   const authenticateUserUseCase = new AuthenticateUserUseCase(
     inMemoryUserDatabase,
-    userSecurityProvider
+    userSecurityProvider,
+    inMemoryRefreshTokenDatabase
   );
 
   it("should be able authenticate", async () => {
@@ -30,6 +33,40 @@ describe("Authenticate user use case", () => {
     strictEqual(inMemoryUserDatabase.users.length, 1);
     ok(authenticate);
     strictEqual(authenticate.user, user);
+    strictEqual(
+      inMemoryRefreshTokenDatabase.refreshTokens[0],
+      authenticate.refreshToken
+    );
+    strictEqual(inMemoryRefreshTokenDatabase.refreshTokens.length, 1);
+  });
+
+  it("should be able to delete the user's existing refresh token when the user re-authenticates", async () => {
+    const user = await new MakeUserFactory().toDomain({
+      inMemoryDatabase: inMemoryUserDatabase,
+      override: {
+        email: "authenticate-user-test@example.com",
+      },
+    });
+
+    const { refreshToken } = await authenticateUserUseCase.execute({
+      email: user.props.email,
+      password: user.props.password,
+    });
+
+    const authenticate = await authenticateUserUseCase.execute({
+      email: user.props.email,
+      password: user.props.password,
+    });
+
+    strictEqual(inMemoryUserDatabase.users[1], authenticate.user);
+    strictEqual(inMemoryUserDatabase.users.length, 2);
+    strictEqual(authenticate.user, user);
+    notStrictEqual(inMemoryRefreshTokenDatabase.refreshTokens[0], refreshToken);
+    strictEqual(
+      inMemoryRefreshTokenDatabase.refreshTokens[0],
+      authenticate.refreshToken
+    );
+    strictEqual(inMemoryRefreshTokenDatabase.refreshTokens.length, 1);
   });
 
   it("should not be able authenticate if provided email not equal that user email", async () => {
