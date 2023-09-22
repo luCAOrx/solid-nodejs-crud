ea3296e0ca278cc0dcb104809289e8e21fdb7859
luCAOrx <lucaorx@protonmail.com>
2023-09-21 23:48:07 -0300

    test: add in-memory database for refresh token

---
+++ test/in-memory-database/in-memory-refresh-token-database.ts
@@ -0,0 +1,32 @@
+import {
+  type RefreshTokenProps,
+  type RefreshTokenRepository,
+} from "@domain/repositories/refresh-token-repository";
+
+export class InMemoryRefreshTokenDatabase implements RefreshTokenRepository {
+  public refreshTokens: RefreshTokenProps[] = [];
+
+  async create(refreshToken: RefreshTokenProps): Promise<RefreshTokenProps> {
+    this.refreshTokens.push(refreshToken);
+
+    return refreshToken;
+  }
+
+  async findById(id: string): Promise<RefreshTokenProps | null> {
+    const refreshToken = this.refreshTokens.find(
+      (refreshToken) => refreshToken.id === id
+    );
+
+    if (refreshToken == null) return null;
+
+    return refreshToken;
+  }
+
+  async delete(id: string): Promise<void> {
+    const refreshToken = this.refreshTokens.findIndex(
+      (refreshToken) => refreshToken.id === id
+    );
+
+    this.refreshTokens.splice(refreshToken, 1);
+  }
+}
