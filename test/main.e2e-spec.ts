cdfe1accfb1bcfa8d075001f45e2fd7f4103d17f
luCAOrx <lucaorx@protonmail.com>
2023-11-04 13:49:15 -0300

    test: Changed console.log to process.stdout.write, added methods to be executed before running tests and added test for the RefreshJwtTokenController class controller

--- test/main.e2e-spec.ts
+++ test/main.e2e-spec.ts
@@ -6,19 +6,42 @@ import { authenticateUserControllerEndToEndTests } from "@infra/http/controllers
 import { deleteUserControllerEndToEndTests } from "@infra/http/controllers/delete-user/delete-user-controller.e2e-spec";
 import { getUserControllerEndToEndTests } from "@infra/http/controllers/get-user/get-user-controller.e2e-spec";
 import { getUsersControllerEndToEndTests } from "@infra/http/controllers/get-users/get-users-controller.e2e-spec";
+import { refreshJwtTokenControllerEndToEndTests } from "@infra/http/controllers/refresh-jwt-token/refresh-jwt-token-controller.e2e-spec";
 import { registerUserControllerEndToEndTests } from "@infra/http/controllers/register-user/register-user-controller.e2e-spec";
 import { updateUserControllerEndToEndTests } from "@infra/http/controllers/update-user/update-user-controller.e2e-spec";
 import { pageNotFoundErrorEndToEndTests } from "@infra/http/errors/page-not-found/page-not-found-error.e2e-spec";
 import { ensureAuthenticatedMiddlewareEndToEndTests } from "@infra/http/middlewares/ensure-authenticated-middleware.e2e-spec";
+
+import {
+  MakeAdminUserFactory,
+  MakeUserFactory,
+} from "./factories/make-user-factory";
 
 const prismaTestEnvironment = new PrismaTestEnvironment();
 const server = app.listen(process.env.TEST_SERVER_PORT);
 
 describe("End to end (E2E) tests", () => {
   before(async () => {
-    console.log("Up database and run migrations");
+    process.stdout.write(
+      `Uploading the database, running migrations, seeds and up the server, wait...\n`
+    );
+
     await prismaTestEnvironment.setup();
     await new Promise((resolve) => server.once("listening", resolve));
+
+    process.stdout.write(`Registering admin user, wait...\n`);
+
+    await new MakeAdminUserFactory().toHttp({});
+
+    process.stdout.write(`Registering 20 users, wait...\n`);
+
+    for (let i = 0; i < 20; i++) {
+      await new MakeUserFactory().toHttp({
+        override: {
+          email: `joe${i}@example.com`,
+        },
+      });
+    }
   });
 
   authenticateUserControllerEndToEndTests();
@@ -29,6 +52,7 @@ describe("End to end (E2E) tests", () => {
   updateUserControllerEndToEndTests();
   pageNotFoundErrorEndToEndTests();
   ensureAuthenticatedMiddlewareEndToEndTests();
+  refreshJwtTokenControllerEndToEndTests();
 
   after(async () => {
     server.close();
