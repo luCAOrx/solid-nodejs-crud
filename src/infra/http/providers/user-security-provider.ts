a582067690ab66d1d81b43ad3ac51126706905cf
luCAOrx <lucaorx@protonmail.com>
2023-11-04 17:38:03 -0300

    feat: Add refreshToken method, and changed hash parameter to hashedPassword in compare method

--- src/infra/http/providers/user-security-provider.ts
+++ src/infra/http/providers/user-security-provider.ts
@@ -7,10 +7,28 @@ import {
   type SecurityProvider,
   type HashProps,
 } from "@domain/providers/security-provider";
+import { type RefreshTokenProps } from "@domain/repositories/refresh-token-repository";
+import { GenerateRefreshToken } from "@domain/utils/refresh-token/generate-refresh-jwt-token";
+
+import { PrismaRefreshTokenRepository } from "../repositories/prisma-refresh-token-repository";
+import { PrismaUserRepository } from "../repositories/prisma-user-repository";
 
 export class UserSecurityProvider implements SecurityProvider {
-  async compare({ password, hash }: CompareProps): Promise<boolean> {
-    return await compare(password, hash);
+  async refreshToken(userId: string): Promise<RefreshTokenProps> {
+    const prismaRefreshTokenRepository = new PrismaRefreshTokenRepository();
+    const prismaUserRepository = new PrismaUserRepository();
+    const generateRefreshToken = new GenerateRefreshToken(
+      prismaRefreshTokenRepository,
+      prismaUserRepository
+    );
+
+    const { refreshToken } = await generateRefreshToken.execute({ userId });
+
+    return refreshToken;
+  }
+
+  async compare({ password, hashedPassword }: CompareProps): Promise<boolean> {
+    return await compare(password, hashedPassword);
   }
 
   sign({ payload, secretOrPrivateKey, expiresIn }: SignProps): string {
