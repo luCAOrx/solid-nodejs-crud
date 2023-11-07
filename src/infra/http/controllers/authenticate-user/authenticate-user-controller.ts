b546fa444312a893262968137db31998c2de8d13
luCAOrx <lucaorx@protonmail.com>
2023-11-07 01:02:34 -0300

    feat: Added prismaRefreshTokenRepository dependency to authenticateUserUseCase, added the refreshToken property to the request response, added verification to prevent the user from sending data that is not the same as what was defined in request.body and changed the status code from 500 to 400

--- src/infra/http/controllers/authenticate-user/authenticate-user-controller.ts
+++ src/infra/http/controllers/authenticate-user/authenticate-user-controller.ts
@@ -3,6 +3,7 @@ import { type Request, type Response } from "express";
 import { AuthenticateUserUseCase } from "@domain/use-cases/authenticate-user/authenticate-user-use-case";
 import { InvalidEmailOrPasswordError } from "@domain/use-cases/authenticate-user/errors/invalid-email-or-password-error";
 import { UserSecurityProvider } from "@infra/http/providers/user-security-provider";
+import { PrismaRefreshTokenRepository } from "@infra/http/repositories/prisma-refresh-token-repository";
 import { PrismaUserRepository } from "@infra/http/repositories/prisma-user-repository";
 import { UserViewModel } from "@infra/http/view-models/user-view-model";
 
@@ -18,9 +19,11 @@ export class AuthenticateUserController {
 
     const prismaUserRepository = new PrismaUserRepository();
     const userSecurityProvider = new UserSecurityProvider();
+    const prismaRefreshTokenRepository = new PrismaRefreshTokenRepository();
     const authenticateUserUseCase = new AuthenticateUserUseCase(
       prismaUserRepository,
-      userSecurityProvider
+      userSecurityProvider,
+      prismaRefreshTokenRepository
     );
 
     await authenticateUserUseCase
@@ -28,10 +31,12 @@ export class AuthenticateUserController {
         email,
         password,
       })
-      .then(({ user, token }) => {
+      .then(({ user, token, refreshToken }) => {
         const userResponse = UserViewModel.toHttp(user);
 
-        return response.status(201).json({ user: userResponse, token });
+        return response
+          .status(201)
+          .json({ user: userResponse, token, refreshToken });
       })
       .catch((error: Error) => {
         if (error instanceof InvalidEmailOrPasswordError) {
@@ -45,13 +50,15 @@ export class AuthenticateUserController {
         if (
           Object.keys(request.body).length === 0 ||
           Object.hasOwn(request.body, "email") ||
-          Object.hasOwn(request.body, "password")
+          Object.hasOwn(request.body, "password") ||
+          !Object.hasOwn(request.body, "email") ||
+          !Object.hasOwn(request.body, "password")
         ) {
-          return response.status(500).json({
-            statusCode: 500,
+          return response.status(400).json({
+            statusCode: 400,
             message:
               "The properties: email and password, should be provided in the request body",
-            error: "Internal Server Error",
+            error: "Bad request",
           });
         }
       });
