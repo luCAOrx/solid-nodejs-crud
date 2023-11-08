ab40fa11a34a328aaa32e0b49dec32d7b210b66b
luCAOrx <lucaorx@protonmail.com>
2023-11-08 02:13:33 -0300

    test: Add authenticate variable with block scope and its value can be reassigned, added user creation and authentication of this user before running the tests, assigned the value of the autheticate variable for the use case of authenticating user, added delay of 16 seconds for the test to update the refresh token, changed the test should be able to return the same jwt token

--- src/domain/use-cases/refresh-jwt-token/refresh-jwt-token-use-case.spec.ts
+++ src/domain/use-cases/refresh-jwt-token/refresh-jwt-token-use-case.spec.ts
@@ -1,6 +1,8 @@
 import { deepStrictEqual, rejects, notDeepStrictEqual } from "node:assert";
-import { describe, it } from "node:test";
+import { describe, it, before } from "node:test";
 
+import { type User } from "@domain/entities/user/user";
+import { type RefreshTokenProps } from "@domain/repositories/refresh-token-repository";
 import { MakeUserFactory } from "@test/factories/make-user-factory";
 import { InMemoryRefreshTokenDatabase } from "@test/in-memory-database/in-memory-refresh-token-database";
 import { InMemoryUserDatabase } from "@test/in-memory-database/in-memory-user-database";
@@ -26,30 +28,54 @@ describe("Generate refresh jwt token use case", () => {
     userSecurityProvider
   );
 
-  it("should be able refresh token", async () => {
+  let authenticate: {
+    user: User;
+    token: string;
+    refreshToken: RefreshTokenProps;
+  };
+
+  before(async () => {
     const user = await new MakeUserFactory().toDomain({
       inMemoryDatabase: inMemoryUserDatabase,
     });
 
-    const authenticate = await authenticateUserUseCase.execute({
+    authenticate = await authenticateUserUseCase.execute({
       email: user.props.email,
-      password: user.props.password,
+      password: "1234567890",
     });
+  });
 
-    const { refreshToken, tokenJwt } = await refreshJwtTokenUseCase.execute({
-      refreshTokenId: authenticate.refreshToken.id,
-    });
+  it("should be able to refresh token", async () => {
+    const sixteenSeconds = 16000;
 
-    deepStrictEqual(
-      inMemoryRefreshTokenDatabase.refreshTokens[0],
-      refreshToken
-    );
-    deepStrictEqual(inMemoryRefreshTokenDatabase.refreshTokens.length, 1);
-    notDeepStrictEqual(authenticate.refreshToken, refreshToken);
-    deepStrictEqual(
-      tokenJwt,
-      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    );
+    await new Promise((resolve) => setTimeout(resolve, sixteenSeconds));
+
+    await refreshJwtTokenUseCase
+      .execute({
+        refreshTokenId: authenticate.refreshToken.id,
+      })
+      .then(async ({ token, refreshToken }) => {
+        await inMemoryRefreshTokenDatabase
+          .findById(authenticate.refreshToken.id)
+          .then((inMemoryRefreshTokenDatabaseResponse) => {
+            deepStrictEqual(inMemoryRefreshTokenDatabaseResponse, null);
+          });
+
+        deepStrictEqual(
+          inMemoryRefreshTokenDatabase.refreshTokens[0],
+          refreshToken
+        );
+        deepStrictEqual(inMemoryRefreshTokenDatabase.refreshTokens.length, 1);
+        deepStrictEqual(refreshToken?.userId, authenticate.user.id);
+        notDeepStrictEqual(refreshToken, authenticate.refreshToken);
+        notDeepStrictEqual(token, authenticate.token);
+
+        authenticate = {
+          user: authenticate.user,
+          refreshToken: Object(refreshToken),
+          token,
+        };
+      });
   });
 
   it("should not be able to refresh token if the refresh token id is not found", async () => {
@@ -60,58 +86,19 @@ describe("Generate refresh jwt token use case", () => {
     }, RefreshTokenNotFoundError);
   });
 
-  it("should be able to delete refresh token if refresh token has expired", async () => {
-    const user = await new MakeUserFactory().toDomain({
-      inMemoryDatabase: inMemoryUserDatabase,
-    });
-
-    const authenticate = await authenticateUserUseCase.execute({
-      email: user.props.email,
-      password: user.props.password,
-    });
-
-    const firstRefreshToken = await refreshJwtTokenUseCase.execute({
-      refreshTokenId: authenticate.refreshToken.id,
-    });
-
-    deepStrictEqual(
-      inMemoryRefreshTokenDatabase.refreshTokens[0],
-      firstRefreshToken.refreshToken
-    );
-    deepStrictEqual(inMemoryRefreshTokenDatabase.refreshTokens.length, 1);
-    notDeepStrictEqual(
-      authenticate.refreshToken,
-      firstRefreshToken.refreshToken
-    );
-    deepStrictEqual(
-      firstRefreshToken.tokenJwt,
-      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    );
-
-    const timeout = setTimeout(() => {}, 20000);
-
-    const secondaryRefreshToken = await refreshJwtTokenUseCase.execute({
-      refreshTokenId: firstRefreshToken.refreshToken.id,
-    });
-
-    deepStrictEqual(
-      inMemoryRefreshTokenDatabase.refreshTokens[0],
-      secondaryRefreshToken.refreshToken
-    );
-    deepStrictEqual(inMemoryRefreshTokenDatabase.refreshTokens.length, 1);
-    deepStrictEqual(
-      firstRefreshToken.tokenJwt,
-      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    );
-    notDeepStrictEqual(
-      authenticate.refreshToken,
-      secondaryRefreshToken.refreshToken
-    );
-    notDeepStrictEqual(
-      inMemoryRefreshTokenDatabase.refreshTokens[0],
-      firstRefreshToken.refreshToken
-    );
-
-    clearTimeout(timeout);
+  it("should be able to return the same jwt token if the refresh token is not expired", async () => {
+    await refreshJwtTokenUseCase
+      .execute({
+        refreshTokenId: authenticate.refreshToken.id,
+      })
+      .then(({ refreshToken, token }) => {
+        deepStrictEqual(
+          inMemoryRefreshTokenDatabase.refreshTokens[0],
+          authenticate.refreshToken
+        );
+        deepStrictEqual(inMemoryRefreshTokenDatabase.refreshTokens.length, 1);
+        deepStrictEqual(refreshToken, undefined);
+        deepStrictEqual(token, authenticate.token);
+      });
   });
 });
