2e5cd618a29f051252d798ee2cd13f7cd7a13fbe
luCAOrx <lucaorx@protonmail.com>
2023-11-07 23:05:21 -0300

    test: Changed assert strictEqual to deepStrictEqual, removed user creation from some tests

--- src/domain/use-cases/register-user/register-user-use-case.spec.ts
+++ src/domain/use-cases/register-user/register-user-use-case.spec.ts
@@ -1,70 +1,61 @@
-import { strictEqual, ok, rejects } from "node:assert";
+import { deepStrictEqual, ok, rejects } from "node:assert";
 import { describe, it } from "node:test";
 
 import { User } from "@domain/entities/user/user";
 import { NameShouldNotBeEmptyError } from "@domain/validations/name/errors/name-should-not-be-empty-error";
+import { MakeUserFactory } from "@test/factories/make-user-factory";
 import { InMemoryUserDatabase } from "@test/in-memory-database/in-memory-user-database";
 import { UserSecurityProvider } from "@test/utils/user-security-provider";
 
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 import { RegisterUserUseCase } from "./register-user-use-case";
 
-const inMemoryUserDatabase = new InMemoryUserDatabase();
-const userSecurityProvider = new UserSecurityProvider();
-const registerUserUseCase = new RegisterUserUseCase(
-  inMemoryUserDatabase,
-  userSecurityProvider
-);
-
 describe("Register user use case", () => {
-  it("should be able to register new user", async () => {
-    const response = await registerUserUseCase.execute({
-      name: "Lucas",
-      job: "Development",
-      email: "lucas@example.com",
-      password: "1234567890",
-    });
+  const inMemoryUserDatabase = new InMemoryUserDatabase();
+  const userSecurityProvider = new UserSecurityProvider();
+  const registerUserUseCase = new RegisterUserUseCase(
+    inMemoryUserDatabase,
+    userSecurityProvider
+  );
 
-    ok(inMemoryUserDatabase.exists("lucas@example.com"));
-    strictEqual(inMemoryUserDatabase.users[0], response.user);
-    strictEqual(inMemoryUserDatabase.users.length, 1);
-    ok(response);
-    strictEqual(response, response);
+  it("should be able to register new user", async () => {
+    await new MakeUserFactory()
+      .toDomain({
+        inMemoryDatabase: inMemoryUserDatabase,
+      })
+      .then((user) => {
+        deepStrictEqual(inMemoryUserDatabase.users[0], user);
+        deepStrictEqual(inMemoryUserDatabase.users.length, 1);
+        ok(user instanceof User);
+      });
   });
 
   it("should not be able to register new user with invalid data", async () => {
     await rejects(
       async () =>
-        await registerUserUseCase.execute({
-          name: "",
-          job: "dev".repeat(260),
-          email: "ana",
-          password: "123",
+        await new MakeUserFactory().toDomain({
+          inMemoryDatabase: inMemoryUserDatabase,
+          override: {
+            name: "",
+            job: "dev".repeat(260),
+            email: "ana",
+            password: "123",
+          },
         }),
       NameShouldNotBeEmptyError
     );
   });
 
   it("should not be able to register new user with existing email", async () => {
-    const user = User.create({
-      name: "Lucas",
-      job: "Development",
-      email: "lucas@example.com",
-      password: "1234567890",
-      role: "COMMON",
-    });
-
-    await inMemoryUserDatabase.create(user);
-
-    await rejects(
-      async () =>
-        await registerUserUseCase.execute({
-          name: "Lucas",
-          job: "Development",
-          email: "lucas@example.com",
-          password: "1234567890",
-        }),
-      UserAlreadyExistsError
-    );
+    await new MakeUserFactory()
+      .toDomain({
+        inMemoryDatabase: inMemoryUserDatabase,
+      })
+      .then(async ({ props }) => {
+        await rejects(
+          async () => await registerUserUseCase.execute(props),
+          UserAlreadyExistsError
+        );
+      });
   });
 });
