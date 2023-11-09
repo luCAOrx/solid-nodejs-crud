0bfda933656351ef6c832a8d67fe022cd38d04fb
luCAOrx <lucaorx@protonmail.com>
2023-11-08 22:39:31 -0300

    test: Changed assert from strictEqual to deepStrictEqual, changed the adminUser variable from const to let, and added the commonUser variable, assigned the administrator user creation value to the adminUser variable, assigned the common user creation value to the commonUser variable, added the .then method to the .execute method of the user search use case, removed the unnecessary creation of users from some tests, changed the userCommon variable to commonUser

--- src/domain/use-cases/get-users/get-users-use-case.spec.ts
+++ src/domain/use-cases/get-users/get-users-use-case.spec.ts
@@ -1,48 +1,50 @@
-import { strictEqual, ok, rejects } from "node:assert";
+import { deepStrictEqual, ok, rejects } from "node:assert";
 import { describe, it, before } from "node:test";
 
 import { type User } from "@domain/entities/user/user";
-import { MakeAdminUserFactory } from "@test/factories/make-user-factory";
+import {
+  MakeAdminUserFactory,
+  MakeUserFactory,
+} from "@test/factories/make-user-factory";
 import { InMemoryUserDatabase } from "@test/in-memory-database/in-memory-user-database";
-import { UserSecurityProvider } from "@test/utils/user-security-provider";
 
 import { UserNotFoundError } from "../errors/user-not-found-error";
-import { RegisterUserUseCase } from "../register-user/register-user-use-case";
 import { AccessDeniedError } from "./errors/access-denied-error";
 import { GetUsersUseCase } from "./get-users-use-case";
 
 describe("Get users use case", () => {
   const inMemoryUserDatabase = new InMemoryUserDatabase();
-  const userSecurityProvider = new UserSecurityProvider();
-  const registerUserUseCase = new RegisterUserUseCase(
-    inMemoryUserDatabase,
-    userSecurityProvider
-  );
 
   const getUsersUseCase = new GetUsersUseCase(inMemoryUserDatabase);
 
-  const userAdmin = new MakeAdminUserFactory().toDomain({
-    inMemoryDatabase: inMemoryUserDatabase,
-  });
+  let adminUser: User;
+  let commonUser: User;
 
   before(async () => {
+    adminUser = new MakeAdminUserFactory().toDomain({
+      inMemoryDatabase: inMemoryUserDatabase,
+    });
+
     for (let i = 0; i < 20; i++) {
-      await registerUserUseCase.execute({
-        name: "Rafael",
-        job: "Actor",
-        email: `fernando${i}@example.com`,
-        password: "1234567890",
+      commonUser = await new MakeUserFactory().toDomain({
+        inMemoryDatabase: inMemoryUserDatabase,
+        override: {
+          email: `joe${i}@example.com`,
+        },
       });
     }
   });
 
   it("should be able to list users if role is admin", async () => {
-    const usersOrUser = await getUsersUseCase.execute({ id: userAdmin.id });
-
-    strictEqual(usersOrUser.length, 5);
-    strictEqual(usersOrUser[0].props.email, "david@example.com");
-    strictEqual(usersOrUser[1].props.email, "fernando0@example.com");
-    ok(usersOrUser instanceof Array<User>);
+    await getUsersUseCase.execute({ id: adminUser.id }).then((response) => {
+      deepStrictEqual(response.length, 5);
+      deepStrictEqual(response[0].props.email, "david@example.com");
+      deepStrictEqual(response[1].props.email, "joe0@example.com");
+      deepStrictEqual(response[2].props.email, "joe1@example.com");
+      deepStrictEqual(response[3].props.email, "joe2@example.com");
+      deepStrictEqual(response[4].props.email, "joe3@example.com");
+      ok(response instanceof Object);
+    });
   });
 
   it("should not be able to list users if user not found", async () => {
@@ -52,38 +54,42 @@ describe("Get users use case", () => {
   });
 
   it("should not be able to list users if role is common", async () => {
-    const userCommon = await registerUserUseCase.execute({
-      name: "Gregory Schmidt",
-      job: "Soccer Player",
-      email: "jimble@example.com",
-      password: "1234567890",
-    });
-
     await rejects(async () => {
-      await getUsersUseCase.execute({ id: userCommon.user.id });
+      await getUsersUseCase.execute({ id: commonUser.id });
     }, AccessDeniedError);
   });
 
   it("should be able paginate", async () => {
-    let usersOrUser = await getUsersUseCase.execute({
-      id: userAdmin.id,
-      takePage: 5,
-    });
+    await getUsersUseCase
+      .execute({
+        id: adminUser.id,
+        page: 1,
+        takePage: 5,
+      })
+      .then((response) => {
+        deepStrictEqual(response.length, 5);
+        deepStrictEqual(response[0].props.email, "david@example.com");
+        deepStrictEqual(response[1].props.email, "joe0@example.com");
+        deepStrictEqual(response[2].props.email, "joe1@example.com");
+        deepStrictEqual(response[3].props.email, "joe2@example.com");
+        deepStrictEqual(response[4].props.email, "joe3@example.com");
+        ok(response instanceof Object);
+      });
 
-    strictEqual(usersOrUser.length, 5);
-    strictEqual(usersOrUser[0].props.email, "david@example.com");
-    strictEqual(usersOrUser[1].props.email, "fernando0@example.com");
-    ok(usersOrUser instanceof Array<User>);
-
-    usersOrUser = await getUsersUseCase.execute({
-      id: userAdmin.id,
-      page: 2,
-      takePage: 5,
-    });
-
-    strictEqual(usersOrUser.length, 5);
-    strictEqual(usersOrUser[0].props.email, "fernando4@example.com");
-    strictEqual(usersOrUser[1].props.email, "fernando5@example.com");
-    ok(usersOrUser instanceof Array<User>);
+    await getUsersUseCase
+      .execute({
+        id: adminUser.id,
+        page: 2,
+        takePage: 5,
+      })
+      .then((response) => {
+        deepStrictEqual(response.length, 5);
+        deepStrictEqual(response[0].props.email, "joe4@example.com");
+        deepStrictEqual(response[1].props.email, "joe5@example.com");
+        deepStrictEqual(response[2].props.email, "joe6@example.com");
+        deepStrictEqual(response[3].props.email, "joe7@example.com");
+        deepStrictEqual(response[4].props.email, "joe8@example.com");
+        ok(response instanceof Object);
+      });
   });
 });
