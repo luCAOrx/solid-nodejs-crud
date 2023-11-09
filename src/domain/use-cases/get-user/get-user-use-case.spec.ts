5d600edf709960e4c47ce9fac21d478cf4b265b0
luCAOrx <lucaorx@protonmail.com>
2023-11-08 22:59:35 -0300

    test: Changed assert from strictEqual to deepStrictEqual, removed unnecessary user creation from some tests, added user variable in block scope to be reassigned its value, added user creation before tests start and assigned the value of this user creation the user variable, added .then method to the execute method of the search user use case

--- src/domain/use-cases/get-user/get-user-use-case.spec.ts
+++ src/domain/use-cases/get-user/get-user-use-case.spec.ts
@@ -1,5 +1,5 @@
-import { strictEqual, ok, rejects } from "node:assert";
-import { describe, it } from "node:test";
+import { deepStrictEqual, ok, rejects } from "node:assert";
+import { describe, it, before } from "node:test";
 
 import { User } from "@domain/entities/user/user";
 import { MakeUserFactory } from "@test/factories/make-user-factory";
@@ -12,57 +12,65 @@ describe("Get user use case", () => {
   const inMemoryUserDatabase = new InMemoryUserDatabase();
   const getUserUseCase = new GetUserUseCase(inMemoryUserDatabase);
 
-  it("should be able get user", async () => {
-    const { id } = await new MakeUserFactory().toDomain({
+  let user: User;
+
+  before(async () => {
+    user = await new MakeUserFactory().toDomain({
       inMemoryDatabase: inMemoryUserDatabase,
     });
+  });
 
-    const { user } = await getUserUseCase.execute({ id });
-
-    strictEqual(inMemoryUserDatabase.users[0].id, user.id);
-    strictEqual(inMemoryUserDatabase.users[0].read_time, 1);
-    strictEqual(inMemoryUserDatabase.users[0], user);
-    strictEqual(inMemoryUserDatabase.users.length, 1);
-    ok(user);
-    strictEqual(user, user);
-    ok(user instanceof User);
+  it("should be able get user", async () => {
+    await getUserUseCase.execute({ id: user.id }).then((response) => {
+      deepStrictEqual(inMemoryUserDatabase.users[0].id, response.user.id);
+      deepStrictEqual(inMemoryUserDatabase.users[0].read_time, 1);
+      deepStrictEqual(inMemoryUserDatabase.users[0], response.user);
+      deepStrictEqual(inMemoryUserDatabase.users.length, 1);
+      ok(response.user);
+      deepStrictEqual(response.user, user);
+      ok(response.user instanceof User);
+    });
   });
 
   it("should be able to count how many times the user has been read", async () => {
-    const { id } = await new MakeUserFactory().toDomain({
-      inMemoryDatabase: inMemoryUserDatabase,
-      override: { name: "Stan March", email: "stan@example.com" },
-    });
+    await getUserUseCase
+      .execute({
+        id: user.id,
+      })
+      .then((response) => {
+        deepStrictEqual(inMemoryUserDatabase.users[0].id, response.user.id);
+        deepStrictEqual(inMemoryUserDatabase.users[0].read_time, 2);
+        deepStrictEqual(inMemoryUserDatabase.users[0], response.user);
+        deepStrictEqual(inMemoryUserDatabase.users.length, 1);
+        ok(response.user);
+        ok(response.user instanceof User);
+      });
 
-    const { user: userReadOneTimes } = await getUserUseCase.execute({ id });
+    await getUserUseCase
+      .execute({
+        id: user.id,
+      })
+      .then((response) => {
+        deepStrictEqual(inMemoryUserDatabase.users[0].id, response.user.id);
+        deepStrictEqual(inMemoryUserDatabase.users[0].read_time, 3);
+        deepStrictEqual(inMemoryUserDatabase.users[0], response.user);
+        deepStrictEqual(inMemoryUserDatabase.users.length, 1);
+        ok(response.user);
+        ok(response.user instanceof User);
+      });
 
-    strictEqual(inMemoryUserDatabase.users[1].id, userReadOneTimes.id);
-    strictEqual(inMemoryUserDatabase.users[1].read_time, 1);
-    strictEqual(inMemoryUserDatabase.users[1], userReadOneTimes);
-    strictEqual(inMemoryUserDatabase.users.length, 2);
-    ok(userReadOneTimes);
-    strictEqual(userReadOneTimes, userReadOneTimes);
-    ok(userReadOneTimes instanceof User);
-
-    const { user: userReadTwoTimes } = await getUserUseCase.execute({ id });
-
-    strictEqual(inMemoryUserDatabase.users[1].id, userReadTwoTimes.id);
-    strictEqual(inMemoryUserDatabase.users[1].read_time, 2);
-    strictEqual(inMemoryUserDatabase.users[1], userReadTwoTimes);
-    strictEqual(inMemoryUserDatabase.users.length, 2);
-    ok(userReadTwoTimes);
-    strictEqual(userReadTwoTimes, userReadTwoTimes);
-    ok(userReadTwoTimes instanceof User);
-
-    const { user: userReadThreeTimes } = await getUserUseCase.execute({ id });
-
-    strictEqual(inMemoryUserDatabase.users[1].id, userReadThreeTimes.id);
-    strictEqual(inMemoryUserDatabase.users[1].read_time, 3);
-    strictEqual(inMemoryUserDatabase.users[1], userReadThreeTimes);
-    strictEqual(inMemoryUserDatabase.users.length, 2);
-    ok(userReadThreeTimes);
-    strictEqual(userReadThreeTimes, userReadThreeTimes);
-    ok(userReadThreeTimes instanceof User);
+    await getUserUseCase
+      .execute({
+        id: user.id,
+      })
+      .then((response) => {
+        deepStrictEqual(inMemoryUserDatabase.users[0].id, response.user.id);
+        deepStrictEqual(inMemoryUserDatabase.users[0].read_time, 4);
+        deepStrictEqual(inMemoryUserDatabase.users[0], response.user);
+        deepStrictEqual(inMemoryUserDatabase.users.length, 1);
+        ok(response.user);
+        ok(response.user instanceof User);
+      });
   });
 
   it("should not be able get user if user not exists", async () => {
