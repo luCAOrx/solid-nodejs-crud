a47d06bfb89da8edce14747b8106d32bd06acade
luCAOrx <lucaorx@protonmail.com>
2023-11-09 00:41:19 -0300

    docs: Changed the name of the property that documented the Internal server error to thePropertiesShouldBeProvidedInTheRequestBody, changed the status code from 500 to 400, and removed the documentation for status code 500

--- docs/src/infra/http/routes/register-user/register-user-response.ts
+++ docs/src/infra/http/routes/register-user/register-user-response.ts
@@ -59,21 +59,16 @@ const httpResponseToClientError: Response = {
             error: "Bad request",
           },
         },
-      },
-    },
-  },
-};
 
-const httpResponseToInternalServerError: Response = {
-  description:
-    "HTTP response to register new user without properties of request body",
-  content: {
-    "application/json": {
-      example: {
-        statusCode: 500,
-        message:
-          "The properties: name, job, email and password, should be provided in the request body",
-        error: "Internal Server Error",
+        thePropertiesShouldBeProvidedInTheRequestBody: {
+          summary: "The properties should be provided in the request body",
+          value: {
+            statusCode: 400,
+            message:
+              "The properties: name, job, email and password, should be provided in the request body",
+            error: "Bad request",
+          },
+        },
       },
     },
   },
@@ -83,6 +78,4 @@ export const registerUserResponse: Responses = {
   201: httpResponseToCreatedResource,
 
   400: httpResponseToClientError,
-
-  500: httpResponseToInternalServerError,
 };
