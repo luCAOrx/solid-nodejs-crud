df4d0f22d0ce59413202b0016c95dd12e5aa7c70
luCAOrx <lucaorx@protonmail.com>
2023-11-09 00:08:44 -0300

    feat: Added exists method

--- src/domain/repositories/refresh-token-repository.ts
+++ src/domain/repositories/refresh-token-repository.ts
@@ -6,6 +6,7 @@ export interface RefreshTokenProps {
 }
 
 export abstract class RefreshTokenRepository {
+  abstract exists(userId: string): Promise<boolean>;
   abstract create(refreshToken: RefreshTokenProps): Promise<RefreshTokenProps>;
   abstract findById(id: string): Promise<RefreshTokenProps | null>;
   abstract delete(id: string): Promise<void>;
