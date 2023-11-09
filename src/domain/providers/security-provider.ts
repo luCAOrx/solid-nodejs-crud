e6c36a1c260ef15c0626a021466c27258bfbf445
luCAOrx <lucaorx@protonmail.com>
2023-11-09 00:13:37 -0300

    refactor: Changed the hash property of the CompareProps interface to hashedPassword, changed the type of the salt property of the HashProps interface to number, changed the response type of the promise of the refreshToken method from string to RefreshTokenProps

--- src/domain/providers/security-provider.ts
+++ src/domain/providers/security-provider.ts
@@ -1,6 +1,8 @@
+import { type RefreshTokenProps } from "@domain/repositories/refresh-token-repository";
+
 export interface CompareProps {
   password: string;
-  hash: string;
+  hashedPassword: string;
 }
 
 export interface SignProps {
@@ -11,12 +13,12 @@ export interface SignProps {
 
 export interface HashProps {
   password: string;
-  salt: string | number;
+  salt: number;
 }
 
 export abstract class SecurityProvider {
   abstract compare(params: CompareProps): Promise<boolean>;
   abstract sign(params: SignProps): string;
   abstract hash(params: HashProps): Promise<string>;
-  abstract refreshToken(userId: string): Promise<string>;
+  abstract refreshToken(userId: string): Promise<RefreshTokenProps>;
 }
