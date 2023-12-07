57adeeeaadb06cb58c9567319a5ebecad1a28817
luCAOrx <lucaorx@protonmail.com>
2023-12-07 00:50:48 -0300

    docs: Add documentation for the password is invalid error, changed all password words to currentPassword and added the word newPassword

--- docs/src/infra/http/routes/update-user/update-user-response.ts
+++ docs/src/infra/http/routes/update-user/update-user-response.ts
@@ -45,6 +45,15 @@ const httpResponseToClientError: Response = {
           },
         },
 
+        theCurrentPasswordIsInvalid: {
+          summary: "The current password is invalid",
+          value: {
+            statusCode: 400,
+            message: "The current password is invalid",
+            error: "Bad request",
+          },
+        },
+
         theFieldNameShouldNotBeEmpty: {
           summary: "The field name should not be empty",
           value: {
@@ -72,12 +81,84 @@ const httpResponseToClientError: Response = {
           },
         },
 
+        theFieldJobShouldNotBeEmpty: {
+          summary: "The field job should not be empty",
+          value: {
+            statusCode: 400,
+            message: "The field job should not be empty",
+            error: "Bad request",
+          },
+        },
+
+        theFieldJobShouldBeThan5Characters: {
+          summary: "The field job should be than 5 characters",
+          value: {
+            statusCode: 400,
+            message: "The field job should be than 5 characters",
+            error: "Bad request",
+          },
+        },
+
+        theFieldJobShouldBeLessThan255Characters: {
+          summary: "The field job should be less than 255 characters",
+          value: {
+            statusCode: 400,
+            message: "The field job should be less than 255 characters",
+            error: "Bad request",
+          },
+        },
+
+        theFieldEmailShouldBeValidEmail: {
+          summary: "The field email should be valid email",
+          value: {
+            statusCode: 400,
+            message: "The field email should be valid email",
+            error: "Bad request",
+          },
+        },
+
+        theFieldEmailShouldBeLessThan255Characters: {
+          summary: "The field email should be less than 255 characters",
+          value: {
+            statusCode: 400,
+            message: "The field email should be less than 255 characters",
+            error: "Bad request",
+          },
+        },
+
+        theFieldNewPasswordShouldNotBeEmpty: {
+          summary: "The field newPassword should not be empty",
+          value: {
+            statusCode: 400,
+            message: "The field newPassword should not be empty",
+            error: "Bad request",
+          },
+        },
+
+        theFieldNewPasswordShouldBeThan10Characters: {
+          summary: "The field newPassword should be than 10 characters",
+          value: {
+            statusCode: 400,
+            message: "The field newPassword should be than 10 characters",
+            error: "Bad request",
+          },
+        },
+
+        theFieldNewPasswordShouldBeLessThan255Characters: {
+          summary: "The field newPassword should be less than 255 characters",
+          value: {
+            statusCode: 400,
+            message: "The field newPassword should be less than 255 characters",
+            error: "Bad request",
+          },
+        },
+
         thePropertiesShouldBeProvidedInTheRequestBody: {
           summary: "The properties should be provided in the request body",
           value: {
             statusCode: 400,
             message:
-              "The properties: name, job, email and password, should be provided in the request body",
+              "The properties: name, job, email, currentPassword, newPassword, should be provided in the request body",
             error: "Bad request",
           },
         },
