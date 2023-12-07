027cc76f3872eb309fef2693583584c4cebced64
luCAOrx <lucaorx@protonmail.com>
2023-12-06 22:27:38 -0300

    chore: Add password_reset_token and password_reset_token_expiration properties

--- src/infra/http/mappers/user-mapper.ts
+++ src/infra/http/mappers/user-mapper.ts
@@ -8,6 +8,8 @@ interface ToPersistenceResponse {
   email: string;
   password: string;
   role: Role;
+  password_reset_token: string;
+  password_reset_token_expiration: Date;
   read_time: number;
   created_at: Date;
   updated_at: Date;
@@ -24,13 +26,17 @@ export class UserMapper {
         role: raw.role,
       },
       raw.id,
-      Number(raw.read_time)
+      Number(raw.read_time),
+      raw.password_reset_token,
+      raw.password_reset_token_expiration
     );
   }
 
   static toPersistence({
     id,
     props: { name, job, email, password, role },
+    password_reset_token,
+    password_reset_token_expiration,
     read_time,
     updated_at,
     created_at,
@@ -43,6 +49,8 @@ export class UserMapper {
       password,
       role,
       read_time,
+      password_reset_token,
+      password_reset_token_expiration,
       created_at,
       updated_at,
     };
