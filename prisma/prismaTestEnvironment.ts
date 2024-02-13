1b8174d76fd71b6cebf47de073bec28cca0b3043
luCAOrx <lucaorx@protonmail.com>
2024-02-13 14:59:49 -0300

    test: Remove unnecessary lines to run tests with sqlite
    This removal was done to reduce the amount of unnecessary code to run tests with sqlite

--- prisma/prismaTestEnvironment.ts
+++ prisma/prismaTestEnvironment.ts
@@ -1,43 +1,14 @@
-import * as dotenv from "dotenv";
 import { execSync } from "node:child_process";
-import { randomUUID } from "node:crypto";
-import { Client } from "pg";
 
-dotenv.config({ path: ".env" });
+import { prisma } from "@infra/http/libs/prisma-client";
 
 export class PrismaTestEnvironment {
-  private readonly schema: string;
-  private readonly connectionString: string;
-
-  constructor() {
-    const dbUser = process.env.DATABASE_USER;
-    const dbPass = process.env.DATABASE_PASSWORD;
-    const dbHost = process.env.DATABASE_HOST;
-    const dbPort = process.env.DATABASE_PORT;
-    const dbName = process.env.DATABASE_NAME;
-
-    this.schema = `test_${randomUUID()}`;
-    this.connectionString = `postgresql://${String(dbUser)}:${String(
-      dbPass
-    )}@${String(dbHost)}:${String(dbPort)}/${String(dbName)}?schema=${
-      this.schema
-    }`;
-  }
-
   async setup(): Promise<void> {
-    process.env.DATABASE_URL = this.connectionString;
-
     execSync("npm run prisma:migrate");
     execSync("npm run prisma:seed");
   }
 
   async teardown(): Promise<void> {
-    const client = new Client({
-      connectionString: this.connectionString,
-    });
-
-    await client.connect();
-    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
-    await client.end();
+    await prisma.user.deleteMany();
   }
 }
