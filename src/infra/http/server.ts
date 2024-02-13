f1c13cccabd6fbe59afe71743e56d9efbeab483a
luCAOrx <lucaorx@protonmail.com>
2024-02-13 14:47:07 -0300

    chore: Add deleteUserAfterThirtyMinutes function
    This function was added as a callback so that when the server is always up, run it once and run it again every 30 minutes

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,3 +1,11 @@
+import { deleteUserAfterThirtyMinutes } from "prisma/deleteUserAfterThirtyMinutes";
+
 import { app } from "./app";
 
-app.listen(process.env.PORT ?? process.env.SERVER_PORT);
+app.listen(process.env.PORT ?? process.env.SERVER_PORT, async () => {
+  await deleteUserAfterThirtyMinutes();
+
+  setInterval(async () => {
+    await deleteUserAfterThirtyMinutes();
+  }, 30 * 60 * 1000);
+});
