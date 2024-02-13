7185d2b990b46e604390d26deb5590e06e5f15a8
luCAOrx <lucaorx@protonmail.com>
2024-02-13 14:51:31 -0300

    chore: Add comparison to avoid insertion conflict
    This comparison was made to correct the insertion conflict that occurred when a user already existed in the database and the seed script was executed

--- prisma/seeds/register-user-seed.ts
+++ prisma/seeds/register-user-seed.ts
@@ -3,37 +3,45 @@ import { hash } from "bcryptjs";
 import { User } from "@domain/entities/user/user";
 import { prisma } from "@infra/http/libs/prisma-client";
 async function main(): Promise<void> {
-  const {
-    id,
-    props: { name, job, email, password, role },
-    read_time,
-    password_reset_token,
-    password_reset_token_expiration,
-    created_at,
-  } = User.create({
-    name: "Admin Name",
-    job: "admin",
-    email: String(process.env.ADMIN_EMAIL),
-    password: String(process.env.ADMIN_PASSWORD),
-    role: "ADMIN",
-  });
-
-  const hashedPassword = await hash(password, 14);
-
-  await prisma.user.create({
-    data: {
-      id,
-      name,
-      job,
-      email,
-      password: hashedPassword,
-      role,
-      password_reset_token,
-      password_reset_token_expiration,
-      read_time,
-      created_at,
+  const userOrNull = await prisma.user.findUnique({
+    where: {
+      email: String(process.env.ADMIN_EMAIL),
     },
   });
+
+  if (userOrNull === null) {
+    const {
+      id,
+      props: { name, job, email, password, role },
+      read_time,
+      password_reset_token,
+      password_reset_token_expiration,
+      created_at,
+    } = User.create({
+      name: "Admin Name",
+      job: "admin",
+      email: String(process.env.ADMIN_EMAIL),
+      password: String(process.env.ADMIN_PASSWORD),
+      role: "ADMIN",
+    });
+
+    const hashedPassword = await hash(password, 14);
+
+    await prisma.user.create({
+      data: {
+        id,
+        name,
+        job,
+        email,
+        password: hashedPassword,
+        role,
+        password_reset_token,
+        password_reset_token_expiration,
+        read_time,
+        created_at,
+      },
+    });
+  }
 }
 
 main()
